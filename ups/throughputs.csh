d0fe627cc934453387d63c2ee3f9152c5c786e7a
ljones <ljones@9508bedb-3015-0410-995b-e0164f5cd44b>
2010-09-23 23:24:21 +0000

    Added csh and sh shell scripts to set environment variables.

---
+++ ups/throughputs.csh
@@ -0,0 +1,6 @@
+setenv dir $PWD
+setenv LSST_THROUGHPUTS_SRD $dir"/srd-1777/"
+setenv LSST_THROUGHPUTS_IMSIM $dir"/imsim/goal/"
+setenv LSST_THROUGHPUTS_ATMOS $dir"/atmos/"
+setenv LSST_THROUGHPUTS_BASELINE $dir"/baseline/"
+setenv LSST_THROUGHPUTS_DEFAULT $LSST_THROUGHPUTS_BASELINE
---
+++ ups/throughputs.sh
@@ -0,0 +1,6 @@
+export dir = $PWD
+export LSST_THROUGHPUTS_SRD = $dir"/srd-1777/"
+export LSST_THROUGHPUTS_IMSIM = $dir"/imsim/goal/"
+export LSST_THROUGHPUTS_ATMOS = $dir"/atmos/"
+export LSST_THROUGHPUTS_BASELINE = $dir"/baseline/"
+export LSST_THROUGHPUTS_DEFAULT = $LSST_THROUGHPUTS_BASELINE
