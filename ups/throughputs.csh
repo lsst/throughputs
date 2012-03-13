7da19500e73007030e5291569c5dc8fbf752ff1d
ljones <ljones@9508bedb-3015-0410-995b-e0164f5cd44b>
2012-03-13 20:51:08 +0000

    added an env variable to throughputs.csh

--- ups/throughputs.csh
+++ ups/throughputs.csh
@@ -4,3 +4,5 @@ setenv LSST_THROUGHPUTS_IMSIM $dir"/imsim/goal/"
 setenv LSST_THROUGHPUTS_ATMOS $dir"/atmos/"
 setenv LSST_THROUGHPUTS_BASELINE $dir"/baseline/"
 setenv LSST_THROUGHPUTS_DEFAULT $LSST_THROUGHPUTS_BASELINE
+setenv SDSS_THROUGHPUTS $dir"/sdss/"
+setenv MEGACAM_THROUGHPUTS $dir"/megacam/"
--- ups/throughputs.sh
+++ ups/throughputs.sh
@@ -4,3 +4,6 @@ export LSST_THROUGHPUTS_IMSIM = $dir"/imsim/goal/"
 export LSST_THROUGHPUTS_ATMOS = $dir"/atmos/"
 export LSST_THROUGHPUTS_BASELINE = $dir"/baseline/"
 export LSST_THROUGHPUTS_DEFAULT = $LSST_THROUGHPUTS_BASELINE
+export SDSS_THROUGHPUTS = $dir"/sdss/"
+export MEGACAM_THROUGHPUTS = $dir"/megacam/"
+
