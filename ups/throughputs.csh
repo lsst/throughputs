cf811ed65e569d86aa2ee7c951e2d7006ab848a8
Lynne Jones <ljones.uw@gmail.com>
2015-03-10 13:11:42 -0700

    Removed some environment variables from package.
    Not all of the environment variables are being used. To avoid littering the shell with too many env variables, only the basics were left.

--- ups/throughputs.csh
+++ ups/throughputs.csh
@@ -1,8 +1,3 @@
 setenv dir $PWD
-#setenv LSST_THROUGHPUTS_SRD $dir"/srd-1777/"
-setenv LSST_THROUGHPUTS_IMSIM $dir"/imsim/actual/"
-setenv LSST_THROUGHPUTS_ATMOS $dir"/atmos/"
 setenv LSST_THROUGHPUTS_BASELINE $dir"/baseline/"
 setenv LSST_THROUGHPUTS_DEFAULT $LSST_THROUGHPUTS_BASELINE
-setenv SDSS_THROUGHPUTS $dir"/sdss/"
-setenv MEGACAM_THROUGHPUTS $dir"/megacam/"
--- ups/throughputs.sh
+++ ups/throughputs.sh
@@ -1,9 +1,4 @@
 export dir = $PWD
-#export LSST_THROUGHPUTS_SRD = $dir"/srd-1777/"
-export LSST_THROUGHPUTS_IMSIM = $dir"/imsim/actual/"
-export LSST_THROUGHPUTS_ATMOS = $dir"/atmos/"
 export LSST_THROUGHPUTS_BASELINE = $dir"/baseline/"
 export LSST_THROUGHPUTS_DEFAULT = $LSST_THROUGHPUTS_BASELINE
-export SDSS_THROUGHPUTS = $dir"/sdss/"
-export MEGACAM_THROUGHPUTS = $dir"/megacam/"
 
