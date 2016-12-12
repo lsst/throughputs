16a950428d69f67a53485ed335ac0306861f4e5d
R. Biswas <rbiswas4@gmail.com>
2016-12-12 15:44:01 -0800

    Responded to review requests 	modified:   approximate_baseline/approximateBandpasses.py

--- approximate_baseline/approximateBandpasses.py
+++ approximate_baseline/approximateBandpasses.py
@@ -1,6 +1,5 @@
 import os
 import numpy as np
-from lsst.utils import getPackageDir
 
 def shorten_wavelengthRange(logthreshold=-250.):
     """
@@ -11,24 +10,28 @@ def shorten_wavelengthRange(logthreshold=-250.):
     Parameters
     ----------
     logthreshold : float, defaults to -250.
-        threshold value such that if the log10(transmission) is less
-        than logthreshold
+        threshold value such that wavlength, transmission pairs are dropped if
+        the log10(transmission) is less than logthreshold
     .. note: This mostly deals with the many consecutive zeros in the lsst
     throughputs, a larger value of the logthreshold will make actual cuts
     """
-    filedir = os.path.join(getPackageDir('throughputs'), 'baseline')
-
+    here = os.path.abspath(__file__)
+    dirname = os.path.dirname(here)
+    packageDir = os.path.split(dirname)[0]
+    filedir = os.path.join(packageDir, 'baseline')
     for bn in 'ugrizy':
         fname = 'total_{}.dat'.format(bn)
         throughputfile = os.path.join(filedir, fname)
+        outfileName = os.path.join(dirname, fname)
         arr = np.loadtxt(throughputfile)
         mask = np.log10(arr[:, 1]) > logthreshold
         minwave = arr[:, 0][mask].min()
         maxwave = arr[:, 0][mask].max()
         mask = (arr[:, 0] > minwave) & (arr[:, 0] < maxwave)
         arr = arr[mask]
-        np.savetxt(fname, arr)
-    
+        if os.path.abspath(outfileName) == os.path.abspath(throughputfile):
+            raise ValueError('Not allowed to write to baseline throughputs')
+        np.savetxt(outfileName, arr)
 
 
 if __name__ == '__main__':
