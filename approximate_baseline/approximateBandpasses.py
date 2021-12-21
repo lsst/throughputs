ff89352893f3478f35b02246b34e14e27fcbe0c4
R. Biswas <rbiswas4@gmail.com>
2021-12-21 21:13:07 +0100

    Updated the script creating approximate throughputs to fix a bug and slightly changed the function.
    The script was designed to only keep parts of the baseline throughuts
    that have bandpass values above a certain threshold in a continuous way.
    This means that it has the throughputs in between the min and max
    wavelengths where the bandpass value is above the threshold.
    
    - In the previous version of the script, we had not included these two
    min_wav and max_wav values which by definition have bandpass values
    above the threshold. This has been fixed.
    
    - Added an optional variable to enable not writing out the files.
    
    - Some trivial changes to the function, and a clearer description of the
    function

--- approximate_baseline/approximateBandpasses.py
+++ approximate_baseline/approximateBandpasses.py
@@ -1,16 +1,19 @@
 import os
 import numpy as np
 
-def shorten_wavelengthRange(logthreshold=-250.):
+def shorten_wavelengthRange(logthreshold=-250., savefile=True):
     """
     reads in the LSST 'total' bandpasses from the baseline directory
     and only keeps those (wavelength, transmission) values for which
-    the transmission is below a given threshold
+    the transmission is below a given threshold. If the transmission values
+    drop below the threshold at wavelengths between the min and max wavelength
+    where the transmission is above the threshold, such (wavelength, transmission)
+    points are included as well.
 
     Parameters
     ----------
     logthreshold : float, defaults to -250.
-        threshold value such that wavlength, transmission pairs are dropped if
+        threshold value such that wavelength, transmission pairs are dropped if
         the log10(transmission) is less than logthreshold
     .. note: This mostly deals with the many consecutive zeros in the lsst
     throughputs, a larger value of the logthreshold will make actual cuts
@@ -19,20 +22,27 @@ def shorten_wavelengthRange(logthreshold=-250.):
     dirname = os.path.dirname(here)
     packageDir = os.path.split(dirname)[0]
     filedir = os.path.join(packageDir, 'baseline')
+
+    throughputs = []
     for bn in 'ugrizy':
         fname = 'total_{}.dat'.format(bn)
         throughputfile = os.path.join(filedir, fname)
         outfileName = os.path.join(dirname, fname)
         arr = np.loadtxt(throughputfile)
         mask = np.log10(arr[:, 1]) > logthreshold
-        minwave = arr[:, 0][mask].min()
-        maxwave = arr[:, 0][mask].max()
-        mask = (arr[:, 0] > minwave) & (arr[:, 0] < maxwave)
-        arr = arr[mask]
-        if os.path.abspath(outfileName) == os.path.abspath(throughputfile):
-            raise ValueError('Not allowed to write to baseline throughputs')
-        np.savetxt(outfileName, arr)
+        minwave = arr[mask][:, 0].min()
+        maxwave = arr[mask][:, 0].max()
 
+        # prevents patchy wavelengths
+        mask = (arr[:, 0] >= minwave) & (arr[:, 0] <= maxwave)
+        arr = arr[mask]
+        throughputs.append(arr)
+        if savefile:
+            if os.path.abspath(outfileName) == os.path.abspath(throughputfile):
+                raise ValueError('Not allowed to write to baseline throughputs')
+            np.savetxt(outfileName, arr)
+
+    return throughputs
 
 if __name__ == '__main__':
     shorten_wavelengthRange(logthreshold=-250.)
